b9dcc009f963f448737fdb38528a709c2f06d12b
Jo-Herman Haugholt <jo-herman@aptoma.com>
2013-10-08 13:57:31 +0200

    Refactor code to conform closer to Ruby best practices.
    Cosmetic changes such as string quotes and concatination,
    but also refactors away the `_cset` method,
    as "private" methods on the global object is a code-smell.

--- lib/capistrano-new-relic/capistrano-new-relic.rb
+++ lib/capistrano-new-relic/capistrano-new-relic.rb
@@ -1,32 +1,25 @@
 require 'capistrano'
 require 'colored'
 
-def _cset(name, *args, &block)
-  unless exists?(name)
-    set(name, *args, &block)
-  end
-end
-
 Capistrano::Configuration.instance.load do
-  _cset(:new_relic_api_key) {
-    abort "Please specify the new_relic_api_key either in :new_relic_api_key or in environment NEW_RELIC_API_KEY".red if ENV['NEW_RELIC_API_KEY'].nil?
+  set(:new_relic_api_key) {
+    abort 'Please specify the new_relic_api_key either in :new_relic_api_key or in environment NEW_RELIC_API_KEY'.red if ENV['NEW_RELIC_API_KEY'].nil?
     ENV['NEW_RELIC_API_KEY']
-  }
+  } unless exists? :new_relic_api_key
 
-  _cset(:new_relic_app_name) { abort "Please specify :newrelic_app_name".red }
-  _cset(:deploy_env) { abort "Please specify :deploy_env, needed for sensible description".red }
-  _cset(:application) { abort "Please specify :application, needed for sensible description".red }
-  _cset(:branch) { abort "Please specify :branch, needed for sensible description".red }
+  [:new_relic_app_name, :deploy_env, :application, :branch].each do |name|
+    set(name) { abort "Please specify :#{name}".red } unless exists? name
+  end
 
   namespace :newrelic do
-    desc "Notify New Relic of deployment"
+    desc 'Notify New Relic of deployment'
     task :notify do
-      local_user = run_locally("git config user.name").strip
-      local_user_email = run_locally("git config user.email").strip
-      current_rev = capture("cd #{latest_release}; git rev-parse HEAD").strip()
-      previous_rev = capture("cd #{previous_release}; git rev-parse HEAD").strip()
+      local_user = run_locally('git config user.name').strip
+      local_user_email = run_locally('git config user.email').strip
+      current_rev = capture("cd #{latest_release}; git rev-parse HEAD").strip
+      previous_rev = capture("cd #{previous_release}; git rev-parse HEAD").strip
       changelog = capture("cd #{latest_release}; git log --oneline #{previous_rev}..#{current_rev}")
-      tmp_file_name = current_rev + '-' + previous_rev + '-changelog'
+      tmp_file_name = "#{current_rev}-#{previous_rev}-changelog"
       File.open(tmp_file_name, 'w') { |file| file.write("deployment[changelog]=#{changelog}")}
       run_locally %Q{\
             curl -s -H "x-api-key:#{new_relic_api_key}"\
